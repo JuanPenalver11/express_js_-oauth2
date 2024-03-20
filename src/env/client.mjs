88ae79ef17d9d0fbb7e16cbce26445021e70557b
juan penalver <juanpenalver@juans-Laptop.local>
2024-03-20 16:21:21 +0700

    somechanges

--- src/env/client.mjs
+++ src/env/client.mjs
@@ -1,4 +1,4 @@
-export const CLIENT_ID ='1216670516937752586'
-export const CLIENT_SECRET = '1OutAKvLqBuBk3D2AozHl42xvnPYP23f'
+export const CLIENT_ID =jjjjj
+export const CLIENT_SECRET = jjj
 
 // https://l4rnrz4l-3000.asse.devtunnels.ms/api/discord/redirect
